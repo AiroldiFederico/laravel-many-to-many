3f7723b83a5aebfc6d3d75d71b8f077a0a88845d
Federico Airoldi <airoldibusiness@gmail.com>
2023-06-25 20:46:16 +0200

    Correzione problema tabella pivot

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use ILLuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -14,6 +16,19 @@ class TechnologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $technologies = [
+            'HTML',
+            'CSS',
+            'Javascript',
+            'PHP'
+        ];
+
+        foreach( $technologies as $elem) {
+
+            $new_tech = new Technology();
+            $new_tech->name = $elem;
+            $new_tech->slug = Str::slug( $new_tech->name );
+            $new_tech->save();
+        }
     }
 }
